607b63765920407379df5196f9a163fbf6a89848
shakyxd <kaungsan1911@gmail.com>
2022-08-19 03:42:26 +0800

    1

---
+++ allcar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Rent Buddy</title>
+
+    </head>
+    <body>
+    <header>
+        <nav>
+            <div>
+                <h3>My Rent Buddy</h3>
+                <ul class="mainmenu">
+                    <li><a href="allcar.php">HOME</a></li>
+                    <li><a href=""></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    </body>
+</html>
